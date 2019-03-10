4f918f385c07bc449f6f8d6f0a63bcce92f48a35
yacineboulyali <yacineboulyalis@gmail.com>
2019-03-10 21:40:46 +0100

    first test

---
+++ index.php
@@ -0,0 +1,314 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Jekyll v3.8.5">
+    <title>Carousel Template · Bootstrap</title>
+    <link rel="canonical" href="https://getbootstrap.com/docs/4.3/examples/carousel/">
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css  ">
+
+</head>
+<body class="min-vh-100">
+<header>
+</header>
+
+<nav class=" container navbar navbar-expand-lg navbar-light">
+    <a class="navbar-brand" href="#"> <img src="images/logo.png" style="width: 60px"></a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-arget="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#"> Agence Web</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Studio Graphique</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link disabled" href="#">Marketing Digital</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link disabled" href="#">  Vidéo et Animation</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link disabled" href="#">Conseils</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link disabled" href="#">ENG</a>
+            </li>
+        </ul>
+    </div>
+</nav>
+
+<main role="main" id="main" >
+    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+        <div class="carousel-inner " role="listbox">
+
+            <div class="carousel-item active ">
+                <div class="d-flex align-items-center justify-content-center min-vh-100">
+                    <div class="display-5">
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-lg-6 col-md-12 col-sm-12 col-12   order-xs-1 order-lg-1 order-2 ">
+                                    <div class="container">
+                                        <div class="row">
+                                            <div class="col-8">
+                                                <div class="mini-logo">
+                                                        <img src="images/content_logo.png"
+                                                             class="img-fluid">
+                                                </div>
+                                            </div>
+                                            <div class="offset-4"></div>
+                                        </div>
+                                        <div class="content-description">
+                                            <p>Comment concevoir une stratégie de communication externe
+                                                ou interne efficace ? Des étapes de réflexion sont indispensables
+                                                : l’étude marketing (état des lieux de la communication,
+                                                étude des cibles…etc.), la réflexion autour de la problématique,
+                                                la planification des actions à mettre en oeuvre, etc.
+                                        </div>
+                                        </p>
+                                        <div>
+                                            <button class="btn btn-brown">I WANT A BRIGHT SOLUTION
+                                            </button>
+                                            <button class="btn btn-blue">Devis Gratuit</button>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-6 col-md-12  text-center col-sm-12 col-12   order-xs-2 order-lg-2 order-1">
+                                    <div class="content ">
+                                        <div class="image-eyes ">
+                                            <img src="images/body5.png">
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="carousel-item ">
+                <div class="d-flex align-items-center justify-content-center min-vh-100">
+                    <div class="display-5">
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-lg-6 col-md-12 col-sm-12 col-12 order-xs-1 order-lg-1 order-2">
+                                    <div class="container">
+                                        <div class="row">
+                                            <div class="col-8">
+                                                <div class="mini-log">
+                                                    <h3 class="content-title">
+                                                        PILOTAGE
+                                                        DE PROJET
+                                                    </h3>
+                                                </div>
+                                            </div>
+                                            <div class="offset-4"></div>
+                                        </div>
+                                        <div class="content-description">
+                                            <p>Comment concevoir une stratégie de communication externe
+                                                ou interne efficace ? Des étapes de réflexion sont indispensables
+                                                : l’étude marketing (état des lieux de la communication,
+                                                étude des cibles…etc.), la réflexion autour de la problématique,
+                                                la planification des actions à mettre en oeuvre, etc.
+                                        </div>
+                                        </p>
+                                        <div>
+                                            <button class="btn btn-brown">I WANT A BRIGHT SOLUTION
+                                            </button>
+                                            <button class="btn btn-blue">Devis Gratuit</button>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-6 col-md-12  text-center col-sm-12 col-12   order-xs-2 order-lg-2 order-1">
+                                    <div class="content">
+                                        <div class="image-eyes align-items-sm-center">
+                                            <img src="images/eyes.png">
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="carousel-item ">
+                <div class="d-flex align-items-center justify-content-center min-vh-100">
+                    <div class="display-5">
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-lg-6 col-md-12 col-sm-12 col-12 order-xs-1 order-lg-1 order-2 ">
+                                    <div class="container">
+                                        <div class="row">
+                                            <div class="col-8">
+                                                <div class="mini-log">
+                                                    <h3 class="content-title">
+                                                        PILOTAGE
+                                                        DE PROJET
+                                                    </h3>
+                                                </div>
+                                            </div>
+                                            <div class="offset-4"></div>
+                                        </div>
+                                        <div class="content-description">
+                                            <p>Comment concevoir une stratégie de communication externe
+                                                ou interne efficace ? Des étapes de réflexion sont indispensables
+                                                : l’étude marketing (état des lieux de la communication,
+                                                étude des cibles…etc.), la réflexion autour de la problématique,
+                                                la planification des actions à mettre en oeuvre, etc.
+                                        </div>
+                                        </p>
+                                        <div>
+                                            <button class="btn btn-brown">I WANT A BRIGHT SOLUTION
+                                            </button>
+                                            <button class="btn btn-blue">Devis Gratuit</button>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-6 col-md-12  text-center col-sm-12 col-12   order-xs-2 order-lg-2 order-1">
+                                    <div class="content">
+                                        <div class="image-eyes align-items-sm-center">
+                                            <img src="images/image-body-one.png">
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+                <div class="carousel-item ">
+                    <div class="d-flex align-items-center justify-content-center min-vh-100">
+                        <div class="display-5">
+                            <div class="container">
+                                <div class="row">
+                                    <div class="col-lg-6 col-md-12 col-sm-12 col-12 order-xs-1 order-lg-1 order-2 ">
+                                        <div class="container">
+                                            <div class="row">
+                                                <div class="col-8">
+                                                    <div class="mini-log">
+                                                        <h3 class="content-title">
+                                                            CRÉATION
+                                                            PRODUCTION
+                                                        </h3>
+                                                    </div>
+                                                </div>
+                                                <div class="offset-4"></div>
+                                            </div>
+                                            <div class="content-description">
+                                                <p>La créativité et la conception graphique
+                                                    sont au coeur de notre métier : c’est ce qui interpelle
+                                                    les cibles ! Notre équipe prend en charge de A à Z la
+                                                    réalisation de vos supports d’édition et web : réflexion,
+                                                    création, exécution, rédaction, relecture, suivi de fabrication,
+                                                    mise en ligne...
+                                            </div>
+                                            </p>
+                                            <div>
+                                                <button class="btn btn-brown">I WANT A BRIGHT
+                                                    SOLUTION
+                                                </button>
+                                                <button class="btn btn-blue">Devis Gratuit</button>
+
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-6 col-md-12  text-center col-sm-12 col-12   order-xs-2 order-lg-2 order-1">
+                                        <div class="content">
+                                            <div class="image-body align-items-sm-center">
+                                                <img src="images/body3.png">
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="carousel-item ">
+                    <div class="d-flex align-items-center justify-content-center min-vh-100">
+                        <div class="display-5">
+                            <div class="container">
+                                <div class="row">
+                                    <div class="col-lg-6 col-md-12 col-sm-12 col-12  order-xs-1 order-lg-1 order-2 ">
+                                        <div class="container">
+                                            <div class="row">
+                                                <div class="col-8">
+                                                    <div class="mini-log">
+                                                        <h3 class="content-title">
+                                                            AGENCEMENT
+                                                            AMÉNAGEMENT
+                                                        </h3>
+                                                    </div>
+                                                </div>
+                                                <div class="offset-4"></div>
+                                            </div>
+                                            <div class="content-description">
+                                                <p>La créativité et la conception graphique sont au coeur de notre
+                                                    métier :
+                                                    c’est ce qui interpelle les cibles ! Notre équipe prend en charge de
+                                                    A à
+                                                    Z
+                                                    la réalisation de vos supports d’édition et web : réflexion,
+                                                    création, exécution, rédaction, relecture, suivi de fabrication,
+                                                    mise en ligne...
+                                            </div>
+                                            </p>
+                                            <div>
+                                                <button  class="btn btn-brown">I WANT A BRIGHT
+                                                    SOLUTION
+                                                </button>
+                                                <button class="btn btn-blue">Devis Gratuit</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-6 col-md-12  text-center col-sm-12 col-12   order-xs-2 order-lg-2 order-1">
+                                        <div class="content">
+                                            <div class="image-body align-items-sm-center">
+                                                <img src="images/body4.png">
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+            <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
+</main>
+
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        crossorigin="anonymous"></script>
+<script>window.jQuery || document.write('<script src="/docs/4.3/assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
+<script src="js/bootstrap.bundle.min.js"
+        integrity="sha384-xrRywqdh3PHs8keKZN+8zzc5TX0GRTLCcmivcbNJWm2rs5C8PRhcEn3czEjhAO9o"
+        crossorigin="anonymous"></script>
+</body>
+</html>
